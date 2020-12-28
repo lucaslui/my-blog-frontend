8d8504aff4f80b5d34d9632c35f82a84ab9ee73b
Lucas Lui Motta <lucasluimotta@gmail.com>
2020-12-27 23:39:48 -0300

    Updates

---
+++ precache-manifest.9fa47855bfd3635a1e559af51f6e441a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023d4a8f4ce85e1f6ea5f400e956c093",
+    "url": "/my-blog-frontend/index.html"
+  },
+  {
+    "revision": "ed2b24f1f035670b7870",
+    "url": "/my-blog-frontend/static/css/main.e21c8687.chunk.css"
+  },
+  {
+    "revision": "f2b47265cd994c1c25f4",
+    "url": "/my-blog-frontend/static/js/2.51b0cfc6.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/my-blog-frontend/static/js/2.51b0cfc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2b24f1f035670b7870",
+    "url": "/my-blog-frontend/static/js/main.90a9287b.chunk.js"
+  },
+  {
+    "revision": "82a3a8d242fa3d68adf0",
+    "url": "/my-blog-frontend/static/js/runtime-main.ea72d6d8.js"
+  },
+  {
+    "revision": "ab20482720806d34f78dde873b2d482a",
+    "url": "/my-blog-frontend/static/media/home.ab204827.jpg"
+  },
+  {
+    "revision": "a8401b91fc7ca73d2964066d3f4c0ed1",
+    "url": "/my-blog-frontend/static/media/profile.a8401b91.png"
+  }
+]);
